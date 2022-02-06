003728d4127810e4cae1fb22246a49d83957e1a7
davepierre23 <soccermsndave@hotmail.com>
2022-02-06 18:19:12 -0500

    done scotia, ameicinExpess

--- americianExpress.py
+++ americianExpress.py
@@ -7,26 +7,9 @@ import pyexcel as p
 
 
 import logging as log
-logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-
+logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 def convertFile(fileName="pcbanking2.csv"):
-    
-    fileName2 = fileName
-    if ".csv"  in fileName2:
-        fileName2=fileName2.replace(".csv", ".xlsx")
-
-
-    wb = Workbook()
-    ws = wb.active
-    with open(fileName, 'r') as f:
-        for row in csv.reader(f):
-            ws.append(row)
-    wb.save(fileName2)
-    wb = load_workbook(fileName2,"rb")
-    ws = wb.active
-    return ws , fileName2
-def convertFile2(fileName="pcbanking2.csv"):
     fileName2 = fileName
     if ".xls"  in fileName2:
         fileName2=fileName2.replace(".xls", ".xlsx")
@@ -38,23 +21,35 @@ def convertFile2(fileName="pcbanking2.csv"):
     ws = wb.active
     return ws , fileName2
 
-def findMaxRows(ws):
-    maxRow = 1
+def findOffsetAndEnd(ws):
+    offSetValue="Date"
+    offSetRow=-1
+    row =1
+    maxRow =-1
+    trackStart =False         
     while (True):
         char = get_column_letter(1)
-        value = ws[char +str(maxRow)].value
-        log.debug(value)
-        if(None == value):
-            maxRow-=1
-            log.debug(maxRow)
-            return maxRow
-        maxRow+=1
+        value = ws[char +str(row)].value
+        log.debug(f"value  {value} at {char +str(row)}")
+       
+        if(offSetValue == value):
+            offSetRow=row+1
+            log.debug(f"offset {offSetRow}")
+            trackStart =True
+        if(trackStart and None == value):
+            maxRow=row
+            log.debug(f"offset {offSetRow} maxRow={maxRow} ")
+            return offSetRow, maxRow
+        
+        row +=1
 
-def findColRows(ws):
+ 
+
+def findColRows(ws,offSetRow):
     maxCol = 1
     while (True):
         char = get_column_letter(maxCol)
-        value = ws[char +str(1)].value
+        value = ws[char +str(offSetRow)].value
         log.debug(value)
         if(None == value):
             maxCol-=1
@@ -63,40 +58,13 @@ def findColRows(ws):
         maxCol+=1
 
 def printEveryLine(ws):
-    for row in  range (1,findMaxRows(ws)):
-        for col in range (1,findColRows(ws)):
+    offSetRow, maxRow=findOffsetAndEnd(ws)
+    for row in  range (offSetRow,maxRow):
+        for col in range (1,findColRows(ws,offSetRow)):
             char = get_column_letter(col)
-            log.info(ws[char +str(row)].value)
+            value= ws[char +str(row)].value
+            log.info(f"value  {value} at {char +str(row)}")
         log.info("")
-
-def checkForInvalidCols(ws,fileName):
-    row =1
-    for col in range (1,findColRows(ws)):
-        char = get_column_letter(col)
-        gridValue=char +str(row)
-        value = ws[gridValue].value
-        if(value == '-'):
-            log.debug("Invalid col ")
-        
-            wb = load_workbook(fileName)
-            ws = wb.active
-            ws.delete_cols(col)
-    return (ws)
-
-def printEveryLine(ws):
-  
-    for row in  range (1,findMaxRows(ws)):
-        for col in range (1,findColRows(ws)):
-            char = get_column_letter(col)
-            log.info(ws[char +str(row)].value)
-        log.info("")
-
-def populateData(ws):
-    for row in  range (1,findMaxRows(ws)):
-        createRow(row,ws)
-        log.info("")
-def deleteCol(ws,col):
-    ws.delete_cols(col)
   
 
 def createRow(row,ws) :
@@ -134,9 +102,10 @@ def createRow(row,ws) :
 
 
 
+
 def main():
-    ws, fileName = convertFile()
-    ws=checkForInvalidCols(ws,fileName)
-    populateData(ws)
+    ws, fileName = convertFile("Summary.xls")
+    ws=printEveryLine(ws)
+    #populateData(ws)
 if __name__ == "__main__":
     main()
